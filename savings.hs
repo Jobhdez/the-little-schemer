b3f149edb2779f3782b90e8e55a66b2a3292c8d6
Job Henandez Lara <hj93@protonmail.com>
2022-11-30 21:09:46 -0800

    first commit

---
+++ savings.hs
@@ -0,0 +1,40 @@
+import qualified Data.Map as Map
+
+type Months = Int
+type HoursWorked = Int
+type HourlyRate = Int
+type TotalSavings = Int
+
+describeIncome :: TotalSavings -> String
+describeIncome savings =
+  "You will save " ++ show savings ++ ":)"
+
+
+computeTaxes :: Months ->  HoursWorked -> HourlyRate -> TotalSavings
+computeTaxes months hrs rate =
+  income - taxes
+  where
+    income = computeIncome months hrs rate
+    taxes = (10/100) * (computeIncome  months hrs rate)
+  
+computeIncome :: Months -> HoursWorked -> HourlyRate -> TotalSavings
+computeIncome months hours rate =
+  months * income
+  where
+    income = (hours * rate) * weeks where
+      weeks = 4
+
+main :: IO ()
+main = do
+  putStrLn "Enter the projected months"
+  months <- getLine
+  putStrLn "Enter the hours worked"
+  hrs <- getLine
+  putStrLn "Enter the hourly rate"
+  rate <- getLine
+  let ms = read months
+  let hours = read hrs
+  let r = read rate
+  let income = computeTaxes ms hours r
+  putStrLn (describeIncome income)
+
