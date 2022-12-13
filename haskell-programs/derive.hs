347263f40aef23ed5d5c9a7fcd66cfe2a9726918
Job Hernandez <hj93@protonmail.com>
2022-12-12 21:05:31 -0800

    add simplification of expressions.

--- haskell-programs/derive.hs
+++ haskell-programs/derive.hs
@@ -13,6 +13,45 @@ derive (CExpr v (PVar op) n) =
         else CExpr (CExpr v (PVar '*') (derive n)) (PVar '+') (CExpr n (PVar '*') (derive v))
     
 
+simplify :: Expr -> Expr
+simplify (PNum n) = (PNum n)
+simplify (PVar v) = (PVar v)
+simplify (CExpr (PVar v) (PVar op) (PNum n)) =
+    if op == '+' 
+        then if n == 0 
+            then (PVar v)
+            else CExpr (PVar v) (PVar op) (PNum n)
+    else if op == '-' 
+        then if n == 0 then (PVar v) else CExpr (PVar v) (PVar op) (PNum n)
+    else if op == '*' then if n == 0 then PNum 0 else CExpr (PVar v) (PVar op) (PNum n)
+    else
+        if n == 1 then (PVar v) else CExpr (PVar v) (PVar op) (PNum n)
+simplify (CExpr (PNum n) (PVar op) (PVar v)) =
+    if op == '+' 
+        then if n == 0 
+            then (PVar v)
+            else CExpr (PVar v) (PVar op) (PNum n)
+    else if op == '-' 
+        then if n == 0 then (PVar v) else CExpr (PVar v) (PVar op) (PNum n)
+    else if op == '*' then if n == 0 then PNum 0 else CExpr (PVar v) (PVar op) (PNum n)
+    else
+        if n == 1 then (PVar v) else CExpr (PVar v) (PVar op) (PNum n)
+simplify (CExpr (PNum n1) (PVar op) (PNum n2)) =
+    if op == '+' 
+        then if n2 == 0 
+            then (PNum n1)
+            else if n1 == 0 then (PNum n2)
+            else (PNum (n1 + n2))
+    else if op == '-' 
+        then if n2 == 0 
+            then (PNum n1)
+            else if n1 == 0 then (PNum (-n2))
+            else (PNum (n1 - n2))
+    else if op == '*' then if n1 == 0 then (PNum 0) else if n2 == 0 then (PNum 0) else PNum (n1 * n2)
+    else
+        if n1 == 1 then (PNum n2) else if n2 == 1 then (PNum n1) else (PNum (n1 * n2))
 
+simplify (CExpr x op y) =
+    CExpr (simplify x) op (simplify y)
 
 
