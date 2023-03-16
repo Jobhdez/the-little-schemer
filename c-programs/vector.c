9722d31e5f88deb1ab16439900000e50fd81b47e
Job <hj93@protonmail.com>
2023-03-15 17:02:27 -0700

    add multiply-polys function.

--- c-programs/vector.c
+++ c-programs/vector.c
@@ -52,6 +52,23 @@ poly *sub_polys(poly *p1, poly *p2) {
   return p3;
 }
 
+poly *multiply_polys(poly *p1, poly *p2) {
+  poly *p3;
+  int p1_size = p1->degree + 1;
+  int p2_size = p2->degree + 1;
+
+  p3->coefficients = malloc(p1_size+p2_size-1);
+  p3->degree = p1->degree + p2->degree;
+
+  for (int i = 0; i < p1_size; i++) {
+    for (int j=0; j<p2_size; j++) {
+      p3->coefficients[i+j] += p1->coefficients[i] *  p2->coefficients[j];
+    }
+  }
+
+  return p3;
+}
+
 poly *derivative(poly *p1) {
   poly *p2;
   p2->degree = p1->degree - 1;
