ff65a2a4fd7c9932167c4b01cd8e3ad3e6542355
Job <hj93@protonmail.com>
2023-04-12 20:47:16 -0700

    add example 3 for another Python program :)

---
+++ x86-64/pycompiler_generated_assembly/example3.s
@@ -0,0 +1,19 @@
+	.globl main
+
+main:
+	pushq %rbp
+	movq %rsp, %rbp
+	subq $32, %rsp
+	movq $3, -8(%rbp)
+	negq -8(%rbp)
+	movq -8(%rbp), %rax
+	movq $10, -16(%rbp)
+	addq -16(%rbp), %rax
+	movq $2, -24(%rbp)
+	movq -24(%rbp), %rsi
+	addq %rsi, %rax
+	movq %rax, %rdi
+	callq print_int
+	addq $32, %rsp
+	popq %rbp
+	retq
