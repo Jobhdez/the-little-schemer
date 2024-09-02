b4e690bea4ae8517e2de7cefe5617216ababd951
Job Hernandez Lara <hj93@protonmail.com>
2024-09-02 15:52:02 -0700

    refactor

--- go-programs/go-serve/vectorServe.go
+++ go-programs/go-serve/vectorServe.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -8,7 +9,7 @@ import (
 	"net/http"
 	"os"
 	"time"
-	"database/sql"
+
 	_ "github.com/lib/pq"
 )
 
@@ -20,9 +21,7 @@ const (
 	dbname   = "golalg"
 )
 
-var (
-	db   *sql.DB
-)
+var db *sql.DB
 
 type Vectors struct {
 	Vector1 []float64 `json:"vector1"`
@@ -41,17 +40,18 @@ type Result struct {
 type MatrixResult struct {
 	MExp [][]float64 `json:"exp"`
 }
+
 var PORT = ":1234"
 
 type VectorAddEntry struct {
-	ID      int      `json:"id"`
+	ID      int       `json:"id"`
 	Vector1 []float64 `json:"vector1"`
 	Vector2 []float64 `json:"vector2"`
 	Result  []float64 `json:"result"`
 }
 
 type MatrixEntry struct {
-	ID      int      `json:"id"`
+	ID      int         `json:"id"`
 	Matrix1 [][]float64 `json:"matrix1"`
 	Matrix2 [][]float64 `json:"matrix2"`
 	Result  [][]float64 `json:"result"`
@@ -64,9 +64,159 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", body)
 }
 
+func makeVectorOpHandler(operation func([]float64, []float64) ([]float64, error), tableName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+
+		d, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusBadRequest)
+			return
+		}
+
+		var vectors Vectors
+		err = json.Unmarshal(d, &vectors)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error parsing JSON", http.StatusBadRequest)
+			return
+		}
+
+		result, err := operation(vectors.Vector1, vectors.Vector2)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		insertQuery := fmt.Sprintf(`INSERT INTO %s (vector1, vector2, result) VALUES ($1, $2, $3)`, tableName)
+		_, err = db.Exec(insertQuery, toJSONString(vectors.Vector1), toJSONString(vectors.Vector2), toJSONString(result))
+		if err != nil {
+			http.Error(w, "Error inserting into database", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(Result{Exp: result})
+	}
+}
+
+func makeMatrixOpHandler(operation func([][]float64, [][]float64) ([][]float64, error), tableName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+
+		d, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusBadRequest)
+			return
+		}
+
+		var matrices Matrices
+		err = json.Unmarshal(d, &matrices)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error parsing JSON", http.StatusBadRequest)
+			return
+		}
+
+		result, err := operation(matrices.Matrix1, matrices.Matrix2)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		insertQuery := fmt.Sprintf(`INSERT INTO %s (matrix1, matrix2, result) VALUES ($1, $2, $3)`, tableName)
+		_, err = db.Exec(insertQuery, toJSONString2D(matrices.Matrix1), toJSONString2D(matrices.Matrix2), toJSONString2D(result))
+		if err != nil {
+			http.Error(w, "Error inserting into database", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(MatrixResult{MExp: result})
+	}
+}
+
+func makeGetAllHandler(query string, scanFunc func(*sql.Rows) (interface{}, error)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
+			return
+		}
+
+		rows, err := db.Query(query)
+		if err != nil {
+			http.Error(w, "Error querying database", http.StatusInternalServerError)
+			return
+		}
+		defer rows.Close()
+
+		var entries []interface{}
+		for rows.Next() {
+			entry, err := scanFunc(rows)
+			if err != nil {
+				http.Error(w, "Error scanning database row", http.StatusInternalServerError)
+				return
+			}
+			entries = append(entries, entry)
+		}
+
+		if err = rows.Err(); err != nil {
+			http.Error(w, "Error iterating over rows", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(entries)
+	}
+}
+
+func scanVectorEntry(rows *sql.Rows) (interface{}, error) {
+	var entry VectorAddEntry
+	var vector1, vector2, result string
+
+	err := rows.Scan(&entry.ID, &vector1, &vector2, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	entry.Vector1 = fromJSONString(vector1)
+	entry.Vector2 = fromJSONString(vector2)
+	entry.Result = fromJSONString(result)
+	return entry, nil
+}
+
+func scanMatrixEntry(rows *sql.Rows) (interface{}, error) {
+	var entry MatrixEntry
+	var matrix1, matrix2, result string
+
+	err := rows.Scan(&entry.ID, &matrix1, &matrix2, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	entry.Matrix1 = fromJSONString2D(matrix1)
+	entry.Matrix2 = fromJSONString2D(matrix2)
+	entry.Result = fromJSONString2D(result)
+	return entry, nil
+}
+
+/* === Linear Algebra === */
+
 func vectorAdd(v1, v2 []float64) ([]float64, error) {
 	if len(v1) != len(v2) {
-		return nil, fmt.Errorf("vectors must be of the same length")
+		return nil, fmt.Errorf("vectors must have the same length")
 	}
 	result := make([]float64, len(v1))
 	for i := range v1 {
@@ -77,7 +227,7 @@ func vectorAdd(v1, v2 []float64) ([]float64, error) {
 
 func vectorSub(v1, v2 []float64) ([]float64, error) {
 	if len(v1) != len(v2) {
-		return nil, fmt.Errorf("vectors must be of the same length")
+		return nil, fmt.Errorf("vectors must have the same length")
 	}
 	result := make([]float64, len(v1))
 	for i := range v1 {
@@ -87,326 +237,21 @@ func vectorSub(v1, v2 []float64) ([]float64, error) {
 }
 
 func matrixAdd(m1, m2 [][]float64) ([][]float64, error) {
-	if len(m1) != len(m2) && len(m1[0]) != len(m2[0]) {
-		return nil, fmt.Errorf("matrices must be of the same length")
+	if len(m1) != len(m2) || len(m1[0]) != len(m2[0]) {
+		return nil, fmt.Errorf("matrices must have the same dimensions")
 	}
-	
 	result := make([][]float64, len(m1))
 	for i := range m1 {
-		result[i] = make([]float64, len(m1[0])) 
-		for j := range m1[0] {
+		result[i] = make([]float64, len(m1[i]))
+		for j := range m1[i] {
 			result[i][j] = m1[i][j] + m2[i][j]
 		}
 	}
 	return result, nil
 }
 
-func matrixSub(m1, m2 [][]float64) ([][]float64, error) {
-	if len(m1) != len(m2) && len(m1[0]) != len(m2[0]) {
-		return nil, fmt.Errorf("matrices must be of the same length")
-	}
-	
-	result := make([][]float64, len(m1))
-	for i := range m1 {
-		result[i] = make([]float64, len(m1[0])) 
-		for j := range m1[0] {
-			result[i][j] = m1[i][j] - m2[i][j]
-		}
-	}
-	return result, nil
-}
-func vectorAddHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	d, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-
-	var vectors Vectors
-	err = json.Unmarshal(d, &vectors)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Error parsing JSON", http.StatusBadRequest)
-		return
-	}
-
-	addResult, err := vectorAdd(vectors.Vector1, vectors.Vector2)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	insertQuery := `INSERT INTO vector_add (vector1, vector2, result) VALUES ($1, $2, $3)`
-	_, err = db.Exec(insertQuery, toJSONString(vectors.Vector1), toJSONString(vectors.Vector2), toJSONString(addResult))
-	if err != nil {
-		http.Error(w, "Error inserting into database", http.StatusInternalServerError)
-		return
-	}
-
-	result := Result{
-		Exp: addResult,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(result)
-}
-
-func getAllVectorAddsHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	rows, err := db.Query("SELECT id, vector1, vector2, result FROM vector_add")
-	if err != nil {
-		http.Error(w, "Error querying database", http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	var entries []VectorAddEntry
-	for rows.Next() {
-		var entry VectorAddEntry
-		var vector1, vector2, result string
-
-		err := rows.Scan(&entry.ID, &vector1, &vector2, &result)
-		if err != nil {
-			http.Error(w, "Error scanning database row", http.StatusInternalServerError)
-			return
-		}
-
-		entry.Vector1 = fromJSONString(vector1)
-		entry.Vector2 = fromJSONString(vector2)
-		entry.Result = fromJSONString(result)
-
-		entries = append(entries, entry)
-	}
-
-	if err = rows.Err(); err != nil {
-		http.Error(w, "Error iterating over rows", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(entries)
-}
-
-func getAllVectorSubssHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	rows, err := db.Query("SELECT id, vector1, vector2, result FROM vector_sub")
-	if err != nil {
-		http.Error(w, "Error querying database", http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	var entries []VectorAddEntry
-	for rows.Next() {
-		var entry VectorAddEntry
-		var vector1, vector2, result string
-
-		err := rows.Scan(&entry.ID, &vector1, &vector2, &result)
-		if err != nil {
-			http.Error(w, "Error scanning database row", http.StatusInternalServerError)
-			return
-		}
-
-		entry.Vector1 = fromJSONString(vector1)
-		entry.Vector2 = fromJSONString(vector2)
-		entry.Result = fromJSONString(result)
-
-		entries = append(entries, entry)
-	}
-
-	if err = rows.Err(); err != nil {
-		http.Error(w, "Error iterating over rows", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(entries)
-}
- 
-func vectorSubHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	d, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-
-	var vectors Vectors
-	err = json.Unmarshal(d, &vectors)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Error parsing JSON", http.StatusBadRequest)
-		return
-	}
-
-
-	subResult, err := vectorSub(vectors.Vector1, vectors.Vector2)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	insertQuery := `INSERT INTO vector_sub (vector1, vector2, result) VALUES ($1, $2, $3)`
-	_, err = db.Exec(insertQuery, toJSONString(vectors.Vector1), toJSONString(vectors.Vector2), toJSONString(subResult))
-	if err != nil {
-		http.Error(w, "Error inserting into database", http.StatusInternalServerError)
-		return
-	}
-
-
-	result := Result{
-		Exp:    subResult,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(result)
-}
-
-func matrixAddHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	d, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-
-	var matrices Matrices
-	err = json.Unmarshal(d, &matrices)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Error parsing JSON", http.StatusBadRequest)
-		return
-	}
-
-	addResult, err := matrixAdd(matrices.Matrix1, matrices.Matrix2)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	
-	insertQuery := `INSERT INTO matrix_add2 (matrix1, matrix2, result) VALUES ($1, $2, $3)`
-	_, err = db.Exec(insertQuery, toJSONString2D(matrices.Matrix1), toJSONString2D(matrices.Matrix2), toJSONString2D(addResult))
-	if err != nil {
-		http.Error(w, "Error inserting into database", http.StatusInternalServerError)
-		return
-	}
-
-	result := MatrixResult{
-                MExp:    addResult,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(result)
-}
-
-func matrixSubHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	d, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusBadRequest)
-		return
-	}
-
-	var matrices Matrices
-	err = json.Unmarshal(d, &matrices)
-	if err != nil {
-		log.Println(err)
-		http.Error(w, "Error parsing JSON", http.StatusBadRequest)
-		return
-	}
-
-	subResult, err := matrixSub(matrices.Matrix1, matrices.Matrix2)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	result := MatrixResult{
-                MExp:    subResult,
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(result)
-}
-
-func getAllMatrixAddsHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-
-	rows, err := db.Query("SELECT id, matrix1, matrix2, result FROM matrix_add2")
-	if err != nil {
-		http.Error(w, "Error querying database", http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	var entries []MatrixEntry
-	for rows.Next() {
-		var entry MatrixEntry
-		var matrix1, matrix2, result string
-
-		err := rows.Scan(&entry.ID, &matrix1, &matrix2, &result)
-		if err != nil {
-			http.Error(w, "Error scanning database row", http.StatusInternalServerError)
-			return
-		}
-
-		entry.Matrix1 = fromJSONString2D(matrix1)
-		entry.Matrix2 = fromJSONString2D(matrix2)
-		entry.Result = fromJSONString2D(result)
-
-		entries = append(entries, entry)
-	}
-
-	if err = rows.Err(); err != nil {
-		http.Error(w, "Error iterating over rows", http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(entries)
-}
+/* === Polynomials === */
+// not yet implemented ...
 
 /* === Utils ==== */
 func toJSONString(data interface{}) string {
@@ -425,16 +270,15 @@ func toJSONString2D(data [][]float64) string {
 	return string(bytes)
 }
 
-
 func fromJSONString2D(data string) [][]float64 {
 	var result [][]float64
 	json.Unmarshal([]byte(data), &result)
 	return result
 }
 
+
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 	var err error
 	db, err = sql.Open("postgres", psqlInfo)
@@ -450,43 +294,38 @@ func main() {
 
 	log.Println("Successfully connected to database!")
 
-	
-	createTableQuery := `CREATE TABLE IF NOT EXISTS vector_add (
-		id SERIAL PRIMARY KEY,
-		vector1 TEXT NOT NULL,
-		vector2 TEXT NOT NULL,
-		result TEXT NOT NULL
-	)`
-	_, err = db.Exec(createTableQuery)
-	if err != nil {
-		log.Fatal("Error creating table:", err)
+	createTables := []string{
+		`CREATE TABLE IF NOT EXISTS vector_add (
+			id SERIAL PRIMARY KEY,
+			vector1 TEXT NOT NULL,
+			vector2 TEXT NOT NULL,
+			result TEXT NOT NULL
+		)`,
+		`CREATE TABLE IF NOT EXISTS vector_sub (
+			id SERIAL PRIMARY KEY,
+			vector1 TEXT NOT NULL,
+			vector2 TEXT NOT NULL,
+			result TEXT NOT NULL
+		)`,
+		`CREATE TABLE IF NOT EXISTS matrix_add2 (
+			id SERIAL PRIMARY KEY,
+			matrix1 TEXT NOT NULL,
+			matrix2 TEXT NOT NULL,
+			result TEXT NOT NULL
+		)`,
 	}
 
-	createTableQuery2 := `CREATE TABLE IF NOT EXISTS vector_sub (
-		id SERIAL PRIMARY KEY,
-		vector1 TEXT NOT NULL,
-		vector2 TEXT NOT NULL,
-		result TEXT NOT NULL
-	)`
-	_, err = db.Exec(createTableQuery2)
-	if err != nil {
-		log.Fatal("Error creating table:", err)
+	for _, createTableQuery := range createTables {
+		_, err = db.Exec(createTableQuery)
+		if err != nil {
+			log.Fatal("Error creating table:", err)
+		}
 	}
 
-	createTableQuery3 := `CREATE TABLE IF NOT EXISTS matrix_add2 (
-		id SERIAL PRIMARY KEY,
-		matrix1 TEXT NOT NULL,
-		matrix2 TEXT NOT NULL,
-		result TEXT NOT NULL
-	)`
-	_, err = db.Exec(createTableQuery3)
-	if err != nil {
-		log.Fatal("Error creating table:", err)
+	if len(os.Args) > 1 {
+		PORT = ":" + os.Args[1]
 	}
-	arguments := os.Args
-	if len(arguments) != 1 {
-		PORT = ":" + arguments[1]
-	}
+
 	mux := http.NewServeMux()
 	s := &http.Server{
 		Addr:         PORT,
@@ -495,20 +334,17 @@ func main() {
 		ReadTimeout:  time.Second,
 		WriteTimeout: time.Second,
 	}
-	mux.Handle("/api/vector/add", http.HandlerFunc(vectorAddHandler))
-	mux.Handle("/api/vector/sub", http.HandlerFunc(vectorSubHandler))
-	mux.Handle("/api/matrix/add", http.HandlerFunc(matrixAddHandler))
-	mux.Handle("/api/matrix/sub", http.HandlerFunc(matrixSubHandler))
-	mux.Handle("/api/vector/add/vectors", http.HandlerFunc(getAllVectorAddsHandler))
-	mux.Handle("/api/vector/sub/vectors", http.HandlerFunc(getAllVectorAddsHandler))
-	mux.Handle("/api/matrix/add/matrices", http.HandlerFunc(getAllMatrixAddsHandler))
 
-	mux.Handle("/", http.HandlerFunc(defaultHandler))
+	mux.Handle("/api/vector/add", makeVectorOpHandler(vectorAdd, "vector_add"))
+	mux.Handle("/api/vector/sub", makeVectorOpHandler(vectorSub, "vector_sub"))
+	mux.Handle("/api/vector/add/vectors", makeGetAllHandler("SELECT * FROM vector_add", scanVectorEntry))
+	mux.Handle("/api/vector/sub/vectors", makeGetAllHandler("SELECT * FROM vector_sub", scanVectorEntry))
 
-	fmt.Println("Ready to serve at", PORT)
-	err = s.ListenAndServe()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	mux.Handle("/api/matrix/add", makeMatrixOpHandler(matrixAdd, "matrix_add2"))
+	mux.Handle("/api/matrix/add/matrices", makeGetAllHandler("SELECT * FROM matrix_add2", scanMatrixEntry))
+
+	mux.HandleFunc("/", defaultHandler)
+
+	log.Println("Listening on port", PORT)
+	log.Fatal(s.ListenAndServe())
 }
